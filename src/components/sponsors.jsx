b85693b2757e08fa5efae241e58c14a53daee874
Bigbilly-githube <abiolatwitter97@gmail.com>
2025-04-03 13:59:12 +0100

    changes

--- src/components/sponsors.jsx
+++ src/components/sponsors.jsx
@@ -11,11 +11,11 @@ function Sponsors (){
         <>
         <section className="w-full h-[128px] bg-black border-t border-slate-700 flex justify-center items-center">
             <div className="w-[90vw] h-[80px]  flex justify-between items-center">
-                <img src={spotify} alt="spotify icon"  className='xl:w-[148px] xl:h-[48px] w-[74px] h-[40px] md:w-[104px] md:h-[60px] sm:w-[84px] sm:h-[60px]'/>
-                <img src={adobe} alt="adobe icon" className='xl:w-[148px] xl:h-[48px] w-[74px] h-[40px] md:w-[104px] md:h-[60px] sm:w-[84px] sm:h-[60px]' />
-                <img src={linear} alt="linear icon" className='xl:w-[148px] xl:h-[48px] w-[74px] h-[40px] md:w-[104px] md:h-[60px] sm:w-[84px] sm:h-[60px]' />
-                <img src={logo1} alt="spotify icon" className='xl:w-[148px] xl:h-[48px] w-[74px] h-[40px] md:w-[104px] md:h-[60px] sm:w-[84px] sm:h-[60px]' />
-                <img src={logo2} alt="spotify icon" className='xl:w-[148px] xl:h-[48px] w-[74px] h-[40px] md:w-[104px] md:h-[60px] sm:w-[84px] sm:h-[60px]' />
+                <img src={spotify} alt="spotify icon"  className='xl:w-[148px] xl:h-[48px] w-[69px] h-[40px] md:w-[104px] md:h-[60px] sm:w-[84px] sm:h-[60px]'/>
+                <img src={adobe} alt="adobe icon" className='xl:w-[148px] xl:h-[48px] w-[69px] h-[40px] md:w-[104px] md:h-[60px] sm:w-[84px] sm:h-[60px]' />
+                <img src={linear} alt="linear icon" className='xl:w-[148px] xl:h-[48px] w-[69px] h-[40px] md:w-[104px] md:h-[60px] sm:w-[84px] sm:h-[60px]' />
+                <img src={logo1} alt="spotify icon" className='xl:w-[148px] xl:h-[48px] w-[69px] h-[40px] md:w-[104px] md:h-[60px] sm:w-[84px] sm:h-[60px]' />
+                <img src={logo2} alt="spotify icon" className='xl:w-[148px] xl:h-[48px] w-[69px] h-[40px] md:w-[104px] md:h-[60px] sm:w-[84px] sm:h-[60px]' />
 
             </div>
 
