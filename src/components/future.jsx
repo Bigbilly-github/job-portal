f427ee957111c705331605d6c48d0511a4827709
Bigbilly-githube <abiolatwitter97@gmail.com>
2025-04-04 16:36:35 +0100

    changes

--- src/components/future.jsx
+++ src/components/future.jsx
@@ -7,10 +7,10 @@ function Future (){
         <section className="w-[100vw] h-[600px] bg-[#FFFFFF80] flex justify-center items-center">
             <div className="w-[90vw] sm:h-[480px] h-[400px] bg-[#000000] rounded-[20px]  flex justify-between  items-center pl-[30px] sm:pl-[65px]">
                 <div className='w-[45vw] md:h-[480px] h-[400px] mt-[85px]'>
-                    <h1 className='w-[438px] md:h-[145px] h-[120px] font-fig font-bold text-[30px] md:text-[50px] text-[#FFFFFF]'>
+                    <h1 className='sm:w-[438px] w-[370px] md:h-[145px] h-[120px] font-fig font-bold text-[30px] md:text-[50px] text-[#FFFFFF]'>
                     Create A Better Future For Yourself
                     </h1>
-                    <p className='md:w-[600px] lg:w-[400px] w-[400px] h-[34px] text-[#FFFFFF] md:mt-[40px] font-fig text-[16px]'>
+                    <p className='md:w-[600px] lg:w-[400px] sm:w-[400px] w-[370px] h-[34px] text-[#FFFFFF] md:mt-[40px] font-fig text-[16px]'>
                     At eu lobortis pretium tincidunt amet lacus ut aenean aliquet. 
                     Blandit a massa elementum id scelerisque rhoncus
                     </p>
