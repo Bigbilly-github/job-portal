f7abea0177b3517044bee5ce8876282d478b8bda
Bigbilly-github <abiolatwitter97@gmail.com>
2025-04-28 19:57:30 +0100

    changes

--- src/components/signupbody.jsx
+++ src/components/signupbody.jsx
@@ -4,7 +4,7 @@ function SignUpBody (){
 
     return(
         <>
-        <section className="bg-[#30968980] sm:h-[90vh] h-[85vh]  flex justify-center items-center">
+        <section className="bg-[#30968980] sm:h-[100vh] h-[90vh]   flex justify-center items-center">
             <div className="xl:h-[87vh] xl:pb-0 sm:pb-[40px] pb-[30px] h-auto lg:w-[50%] sm:w-[70%] w-[80%] border rounded-[20px] flex flex-col items-center sm:pt-[30px] pt-[20px] bg-slate-900">
                 <h1 className=" w-[90%] text-center font-fig font-bold  lg:text-[40px] md:text-[35px]  sm:text-[30px] text-[20px] text-white">
                     Welcome to Job Portal
@@ -14,20 +14,20 @@ function SignUpBody (){
 
                 </p>
                 <div className="flex flex-col gap-[12px] mt-[30px] w-[80%] ">
-                    <label htmlFor="name" className="text-white font-fig lg:text-[20px] md:text-[18px] sm:text-[16px] text-[14px]">Your Name:</label>
-                    <input type="text" name="name" id="name"  className="sm:py-[12px] py-[6px] px-[12px] w-[100%]  rounded-[6px] bg-slate-400" placeholder="James Harden" required/>
+                    <label htmlFor="name" className="text-white font-fig lg:text-[20px] md:text-[18px] sm:text-[16px] text-[12px]">Your Name:</label>
+                    <input type="text" name="name" id="name"  className="sm:py-[12px] py-[3px] px-[12px] w-[100%]  rounded-[6px] bg-slate-400" placeholder="James Harden" required/>
 
                 </div>
 
                 <div className="flex flex-col gap-[12px] mt-[30px]  w-[80%] ">
-                    <label htmlFor="email" className="text-white font-fig lg:text-[20px] md:text-[18px] sm:text-[16px] text-[14px]">Your Email:</label>
-                    <input type="email" name="email" id="email"  className="sm:py-[12px] py-[6px]  px-[12px] w-[100%] rounded-[6px] bg-slate-400" placeholder="email@gmail.com" required/>
+                    <label htmlFor="email" className="text-white font-fig lg:text-[20px] md:text-[18px] sm:text-[16px] text-[12px]">Your Email:</label>
+                    <input type="email" name="email" id="email"  className="sm:py-[12px] py-[3px]  px-[12px] w-[100%] rounded-[6px] bg-slate-400" placeholder="email@gmail.com" required/>
 
                 </div>
 
                 <div className="flex flex-col gap-[12px] mt-[30px]  w-[80%] ">
-                    <label htmlFor="pword" className="text-white font-fig lg:text-[20px] md:text-[18px] sm:text-[16px] text-[14px]">Enter Password:</label>
-                    <input type="password" name="pword" id="pword"  className="sm:py-[12px] py-[6px] px-[12px] w-[100%] rounded-[6px] bg-slate-400" placeholder="***************" required/>
+                    <label htmlFor="pword" className="text-white font-fig lg:text-[20px] md:text-[18px] sm:text-[16px] text-[12px]">Enter Password:</label>
+                    <input type="password" name="pword" id="pword"  className="sm:py-[12px] py-[3px] px-[12px] w-[100%] rounded-[6px] bg-slate-400" placeholder="***************" required/>
 
                 </div>
 
--- src/components/signupheader.jsx
+++ src/components/signupheader.jsx
@@ -4,7 +4,7 @@ function SignUpHeader (){
 
     return(
         <>
-        <header className='bg-black h-[80px] xl:pl-[70px] pl-[30px] sm:bg-[red] md:bg-[blue] lg:bg-[green] xl:bg-[orange] flex items-center'>
+        <header className=' bg-black h-[80px] xl:pl-[70px] pl-[30px] sm:bg-[red] md:bg-[blue] lg:bg-[green] xl:bg-[orange] flex items-center'>
             <div className='flex gap-[10px]'>
                 <img src={logo} alt="job portal icon" />
                 <p className='font-fig text-[20px] font-semibold text-[#FFFFFF]'>
