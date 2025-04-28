a6c5b51cc0e7fb4f185ee5cdeb833f6bc3540ea5
Bigbilly-github <abiolatwitter97@gmail.com>
2025-04-28 20:17:22 +0100

    changes

--- src/components/signupbody.jsx
+++ src/components/signupbody.jsx
@@ -6,7 +6,7 @@ function SignUpBody (){
         <>
         <section className="bg-[#30968980] lg:h-[90vh] sm:h-[80vh] h-[80vh]   flex justify-center items-center">
             <div className="xl:h-[87vh] xl:pb-0 sm:pb-[40px] pb-[30px] h-auto lg:w-[50%] sm:w-[70%] w-[80%] border rounded-[20px] flex flex-col items-center sm:pt-[30px] pt-[20px] bg-slate-900">
-                <h1 className=" w-[90%] text-center font-fig font-bold  lg:text-[40px] md:text-[35px]  sm:text-[30px] text-[20px] text-white">
+                <h1 className=" w-[90%] text-center font-fig font-bold  lg:text-[40px] md:text-[35px]  sm:text-[30px] text-[18px] text-white">
                     Welcome to Job Portal
                 </h1>
                 <p className=" w-[90%] font-fig  text-center  md:text-[16px] sm:text-[14px] text-[12px] text-white">
@@ -15,19 +15,19 @@ function SignUpBody (){
                 </p>
                 <div className="flex flex-col gap-[12px] mt-[30px] w-[80%] ">
                     <label htmlFor="name" className="text-white font-fig lg:text-[20px] md:text-[18px] sm:text-[16px] text-[12px]">Your Name:</label>
-                    <input type="text" name="name" id="name"  className="sm:py-[12px] py-[3px] px-[12px] w-[100%]  rounded-[6px] bg-slate-400" placeholder="James Harden" required/>
+                    <input type="text" name="name" id="name"  className="sm:py-[12px] py-[3px] px-[12px] w-[100%]  rounded-[6px] bg-slate-100" placeholder="James Harden" required/>
 
                 </div>
 
                 <div className="flex flex-col gap-[12px] mt-[30px]  w-[80%] ">
                     <label htmlFor="email" className="text-white font-fig lg:text-[20px] md:text-[18px] sm:text-[16px] text-[12px]">Your Email:</label>
-                    <input type="email" name="email" id="email"  className="sm:py-[12px] py-[3px]  px-[12px] w-[100%] rounded-[6px] bg-slate-400" placeholder="email@gmail.com" required/>
+                    <input type="email" name="email" id="email"  className="sm:py-[12px] py-[3px]  px-[12px] w-[100%] rounded-[6px] bg-slate-100" placeholder="email@gmail.com" required/>
 
                 </div>
 
                 <div className="flex flex-col gap-[12px] mt-[30px]  w-[80%] ">
                     <label htmlFor="pword" className="text-white font-fig lg:text-[20px] md:text-[18px] sm:text-[16px] text-[12px]">Enter Password:</label>
-                    <input type="password" name="pword" id="pword"  className="sm:py-[12px] py-[3px] px-[12px] w-[100%] rounded-[6px] bg-slate-400" placeholder="***************" required/>
+                    <input type="password" name="pword" id="pword"  className="sm:py-[12px] py-[3px] px-[12px] w-[100%] rounded-[6px] bg-slate-100" placeholder="***************" required/>
 
                 </div>
 
